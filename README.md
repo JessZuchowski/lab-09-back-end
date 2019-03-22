5cb3bf9cc4c71b0325d85b7bcaa43bc30258671f
jaezuchowski <jess.zuchowski@gmail.com>
2019-03-22 09:08:13 -0700

    scaffold and day 8 code

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
 # lab-09-back-end
+lab-06-back-end back end for city explorer app
+
+Version: 1.0.0
+
+Project Name Day 1 Authors: Jagdeep Singh & Trey Herndon (increment the patch/fix version number if you make more commits past your first submission) 
+Day 2 Authors: Jesse Van Volkinburg & Trey Herndon 
+Day 3 & 4 Authors: Jess Zuchowski & Trey Herndon
+
+Overview This site is being built so that we can get hands on experience with backend systems and produce a product that pulls from multiple databases and updates the information pulled regularly.
+
+Getting Started Location-route step 1: Clone base code from our Repo. step 2: In terminal window 'npm install dotenv' and create '.env' file containing PORT='port number you use'. step 3: Run nodemon and test routes (location) and outputs. step 4: deploy if desired.
+
+Architecture Langauges currently in use include, JavaScript, node, JSON, npm, SQL. NPM packages in use: express, cors, dotenv, superagent, pg. API keys from Google, Dark Sky, Movie Database, Yelp, MeetUp, and Hiking.
+
+Change Log 2019-03-19 12:20 - Application now has a fully-functional express server, with a GET route for the /location resource. 2019-03-20 12:20 - Application has a /weather GET route and a /meetups GET route, and an error handler.
+
+Credits and Collaborations Credit should be given to CodeFellows for providing all of the frontend javascript, html, and CSS. CodeFellows also gave guidance on the creation of the backend portion.
+
+DAY 1 Number and name of feature: Locations
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 9am
+
+Finish time: 10am
+
+Actual time needed to complete: 1 hour
+
+Number and name of feature: Weather
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 10am
+
+Finish time: 11:15am
+
+Actual time needed to complete: 1:15 minutes
+
+Number and name of feature: Errors
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 11:15am
+
+Finish time: 12:20pm
+
+Actual time needed to complete: 1:05 minutes
+
+DAY 2 Number and name of feature: 1, 2, and 3 Data, Weather and Locations
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 9:15 Finish time: 10:20
+
+Actual time: 1:05 hours
+
+Number and name of feature: 4 Meetups
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 10:35 Finish time: 12:20
+
+Actual time: 1:45 hours
+
+DAY 3 Number and name of feature: 1 Database
+
+Estimate of time needed to complete: 1 hour
+
+Start time: 9:15
+
+Actual time: 50 min
+
+End time: 10:05am
---
+++ server.js
@@ -0,0 +1,158 @@
+'use strict';
+
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const superagent = require('superagent');
+const pg = require('pg');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+
+
+// location route, returns location object
+// Keys: search_query, formatted_query, latitude and longitude
+app.get('/location', getLocation);
+
+// weather route, returns an array of forecast objects
+// Keys: forecast, time
+app.get('/weather', getWeather);
+
+
+
+// TODO: create a getMeetups function
+// [ { link:,
+// name:,
+// creation_date:,
+// host:}, ]
+// app.get('/meetups', getMeetups);
+app.get('/meetups', getMeetups);
+
+// TODO: create a getYelp function
+// app.get('/yelp', getYelp);
+
+// '*' route for invalid endpoints
+app.use('*', (req, res) => res.send('Sorry, that route does not exist'));
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+
+//Create the client connection to the database
+const client = new pg.Client(process.env.HEROKU_POSTGRESQL_TEAL_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+
+//Error handler for when a 500 error happens
+function handleError(err, res) {
+  console.error(err);
+  if (res) res.status(500).send('Sorry, something went wrong');
+}
+
+// HELPER FUNCTIONS
+
+// takes search request and convert to location object
+//location refactored for SQL
+function getLocation(req, res) {
+  let query = req.query.data;
+
+  //defining search query
+  let sql = `SELECT * FROM locations WHERE search_query=$1;`
+  let values = [query];
+
+
+  //making query of database
+  client.query(sql, values)
+    .then(result => {
+      //if the location is in the database, return it to the front end
+      if (result.rowCount > 0) {
+        console.log('LOCATION FROM SQL');
+        res.send(result.rows[0]);
+      } else {
+        //otherwise go get data from APi
+        const mapsURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_MAPS_API_KEY}&address=${req.query.data}`;
+        superagent.get(mapsURL)
+          .then(data => {
+            console.log('LOCATION FROM API');
+            //throw an error if there is a problem with the API
+            if (!data.body.results.length) { throw 'No Data' }
+            //if there is data:
+            else {
+              let location = new Location(query, data.body.results[0]);
+              //creata a query string to add the location data to SQL
+              let newSql = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING id;`;
+              let newValues = Object.values(location);
+              //insert location data into the database and return the unique id for the new record
+              client.query(newSql, newValues)
+                .then(result => {
+                  //attach the returned id onto the location object
+                  location.id = result.rows[0].id;
+                  //return the location data to the front end
+                  res.send(location);
+                })
+            }
+          })
+          .catch(error => handleError(error, res));
+      }
+    })
+}
+
+// function getLocation(req, res) {
+//   const mapsURL = `https://maps.googleapis.com/maps/api/geocode/json?key=${process.env.GOOGLE_MAPS_API_KEY}&address=${req.query.data}`;
+//   return superagent.get(mapsURL)
+//     .then(result => {
+//       res.send(new Location(result.body.results[0], req.query.data));
+//     })
+//     .catch(error => handleError(error));
+// }
+
+// returns array of daily forecasts
+function getWeather(req, res) {
+  const dark_sky_url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API_KEY}/${req.query.data.latitude},${req.query.data.longitude}`;
+
+  return superagent.get(dark_sky_url)
+    .then( weatherResult => {
+      const weatherSummaries = weatherResult.body.daily.data.map((day) => {
+        return new Forecast(day);
+      });
+      res.send(weatherSummaries);
+    })
+    .catch(error => handleError(error));
+}
+
+// returns array of 20 meetup objects
+function getMeetups(req, res) {
+  const meetupUrl = `https://api.meetup.com/find/upcoming_events?lat=${req.query.data.latitude}&lon=${req.query.data.longitude}&sign=true&key=${process.env.MEETUP_API_KEY}&page=20`;
+
+  return superagent.get(meetupUrl)
+    .then( meetupResults => {
+      const meetupList = meetupResults.body.events.map((event) => {
+        return new MeetupEvent(event);
+      });
+      res.send(meetupList);
+    })
+    .catch(error => handleError(error));
+}
+
+// Location object constructor
+function Location(query, data) {
+  this.search_query = query;
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitude = data.geometry.location.lng;
+}
+
+// Forecast object constructor
+function Forecast(day) {
+  this.forecast = day.summary;
+  this.time = new Date(day.time*1000).toString().slice(0,15);
+}
+
+// Meetup event object constructor
+function MeetupEvent(event) {
+  this.link = event.link;
+  this.name = event.name;
+  this.creation_date = new Date(event.time).toString().slice(0, 15);
+  this.host = event.group.name;
+}
