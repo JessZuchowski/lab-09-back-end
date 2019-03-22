af6092a9f948b83615bfcb8ab8670c9c597ad6c0
herndont <herndontrey@gmail.com>
2019-03-22 13:09:48 -0700

    meetups complete

--- schema.sql
+++ schema.sql
@@ -17,3 +17,12 @@ CREATE TABLE weathers (
   location_id INTEGER NOT NULL,
   FOREIGN KEY (location_id) REFERENCES locations (id)
 );
+
+CREATE TABLE meetups (
+  id SERIAL PRIMARY KEY,
+  link VARCHAR(255),
+  name VARCHAR(255),
+  creation_date BIGINT,
+  host VARCHAR(255),
+  FOREIGN KEY (location_id) REFERENCES locations (id;)
+);
